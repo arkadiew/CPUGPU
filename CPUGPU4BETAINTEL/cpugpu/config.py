5d330b46a4fa4a0461d92440b5f932e09ceda3a6
arkadiew <113170277+arkadiew@users.noreply.github.com>
2022-09-18 00:08:10 +0300

    f
    f

---
+++ CPUGPU4BETAINTEL/cpugpu/config.py
@@ -0,0 +1,8 @@
+#APPLICATION ID
+ID = ""
+#large_image
+LI = ""
+#SMALL IMAGE 
+SI = ''
+
+
---
+++ CPUGPU4BETAINTEL/test/config.py
@@ -0,0 +1,8 @@
+#APPLICATION ID
+ID = "983443077073076275"
+#large_image
+LI = "depositphotos_7926477-stock-photo-new-potato"
+#SMALL IMAGE 
+SI = 'peach'
+
+
